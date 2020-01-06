2610d2852db5cf27ba5abf11cc512ea0069a26a1
IN-DF-019 <IN-DF-019@192.168.1.99>
2020-01-06 10:18:24 +0100

    webprojectmaven

---
+++ webprojectmaven/src/main/java/fr/formation/inti/servlet/HelloServlet.java
@@ -0,0 +1,54 @@
+package fr.formation.inti.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.append("<h1>Hello Nina</h1>");
+		out.write("<ul><li>yop</li></ul>");
+		String name = (String) request.getAttribute("name");
+		//Récupère la valeur "test" grâce à la clé "name"
+		out.append("<h3>ForwardHelloServlet " + name + "</h3>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// Utilisé à chaque fois que le contexte est rechargé
+		super.destroy();
+		System.out.println("fin servlet");
+	}
+
+}
