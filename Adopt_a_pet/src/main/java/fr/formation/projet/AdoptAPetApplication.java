9e1d91a99f5621a5ad89333ec830b75b0df8b8df
IN-DF-019 <IN-DF-019@192.168.1.99>
2020-01-09 12:34:53 +0100

    premier commit

---
+++ Adopt_a_pet/src/main/java/fr/formation/projet/AdoptAPetApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptAPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptAPetApplication.class, args);
+	}
+
+}
