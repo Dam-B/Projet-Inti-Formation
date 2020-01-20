90d8dc6960088423f8e1accbad7b42a5086d701f
Damien <bertinaria.damien@gmail.com>
2020-01-20 17:16:02 +0100

    Commit fin de journée

--- Adopte_A_Pet/src/main/java/fr/formation/inti/validation/FormValidator.java
+++ Adopte_A_Pet/src/main/java/fr/formation/inti/validation/FormValidator.java
@@ -5,24 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
+import fr.formation.inti.Iservices.IIndividualsService;
+import fr.formation.inti.entities.Individual;
 
-import fr.formation.inti.entities.Employee;
-import fr.formation.inti.services.EmployeeServiceImpl;
 
 @Component
 public class FormValidator {
 	@Autowired
-	private Inddivi emp;
+	private IIndividualsService indivService;
 
 	// common-validator library.
 	private EmailValidator emailValidator = EmailValidator.getInstance();
 
 	public boolean supports(Class<?> aClass) {
-		return Employee.class.equals(aClass);
+		return Individual.class.equals(aClass);
 	}
 
 	public void validate(Object target, Errors errors) {
-		Employee empForm = (Employee) target;
+		Individual indivForm = (Individual) target;
 
 		// Check the fields of AppUserForm.
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "NotEmpty.appUserForm.firstName");
@@ -33,12 +33,12 @@ public class FormValidator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "NotEmpty.appUserForm.title");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "manager", "NotEmpty.appUserForm.manager");
 
-		if (!this.emailValidator.isValid(empForm.getEmail())) {
+		if (!this.emailValidator.isValid(indivForm.getMail())) {
 			// Invalid email.
 			errors.rejectValue("email", "Pattern.appUserForm.email");
-		} else if (empForm.getEmpId() == null) {
-			Employee dbUser = emp.findByMail(empForm.getEmail());
-			if (dbUser != null) {
+		} else if (indivForm.getUser() == null) {
+			Individual bddIndiv = indivService.findByMail(indivForm.getMail());
+			if (bddIndiv != null) {
 				// Email has been used by another account.
 				errors.rejectValue("email", "Duplicate.appUserForm.email");
 			}
