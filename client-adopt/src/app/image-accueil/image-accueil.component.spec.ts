5683ab7707a0b5e6bfc2d67880aba7a05e7d6652
NinChr <chretien.ninasuzanne@gmail.com>
2020-01-22 17:26:40 +0100

    uite commit precedent

---
+++ client-adopt/src/app/image-accueil/image-accueil.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImageAccueilComponent } from './image-accueil.component';
+
+describe('ImageAccueilComponent', () => {
+  let component: ImageAccueilComponent;
+  let fixture: ComponentFixture<ImageAccueilComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ImageAccueilComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImageAccueilComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client-adopt/src/app/image-accueil/image-accueil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-accueil',
+  templateUrl: './image-accueil.component.html',
+  styleUrls: ['./image-accueil.component.css']
+})
+export class ImageAccueilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
