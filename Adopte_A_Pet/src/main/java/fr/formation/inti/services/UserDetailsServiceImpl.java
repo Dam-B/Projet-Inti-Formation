831902fb551af14f515790e1fa6de9291eed70f6
NinChr <chretien.ninasuzanne@gmail.com>
2020-01-24 09:44:04 +0100

    changement de sign-in en registration

--- Adopte_A_Pet/src/main/java/fr/formation/inti/services/UserDetailsServiceImpl.java
+++ Adopte_A_Pet/src/main/java/fr/formation/inti/services/UserDetailsServiceImpl.java
@@ -1,6 +1,51 @@
-<<<<<<< HEAD
+//<<<<<<< HEAD
+////package fr.formation.inti.services;
+////
+////import java.util.HashSet;
+////import java.util.List;
+////import java.util.Set;
+////
+////import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.security.core.GrantedAuthority;
+////import org.springframework.security.core.authority.SimpleGrantedAuthority;
+////import org.springframework.security.core.userdetails.UserDetails;
+////import org.springframework.security.core.userdetails.UserDetailsService;
+////import org.springframework.security.core.userdetails.UsernameNotFoundException;
+////import org.springframework.stereotype.Service;
+////import org.springframework.transaction.annotation.Transactional;
+////
+////import fr.formation.inti.dao.ITitleRepository;
+////import fr.formation.inti.dao.IUserRepository;
+////import fr.formation.inti.entities.Title;
+////import fr.formation.inti.entities.User;
+////
+////@Service
+////public class UserDetailsServiceImpl implements UserDetailsService{
+//////    @Autowired
+//////    private IUserRepository userRepository;
+//////    @Autowired
+//////    private ITitleRepository titleRepository;
+//////
+//////    @Override
+//////    @Transactional(readOnly = true)
+//////    public UserDetails loadUserByUsername(String username) {
+//////        User user = userRepository.findByUsername(username);
+//////        List<Title> title = titleRepository.findAll();
+//////        
+//////        if (user == null) throw new UsernameNotFoundException(username);
+//////
+//////        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+//////        for (Title title1 : title){
+//////            grantedAuthorities.add(new SimpleGrantedAuthority(title1.getTitle()));
+//////        }
+//////
+//////        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+//////    }
+////}
+//=======
 //package fr.formation.inti.services;
 //
+//import java.util.ArrayList;
 //import java.util.HashSet;
 //import java.util.List;
 //import java.util.Set;
@@ -20,87 +65,42 @@
 //import fr.formation.inti.entities.User;
 //
 //@Service
-//public class UserDetailsServiceImpl implements UserDetailsService{
-////    @Autowired
-////    private IUserRepository userRepository;
-////    @Autowired
-////    private ITitleRepository titleRepository;
-////
-////    @Override
-////    @Transactional(readOnly = true)
-////    public UserDetails loadUserByUsername(String username) {
-////        User user = userRepository.findByUsername(username);
-////        List<Title> title = titleRepository.findAll();
-////        
-////        if (user == null) throw new UsernameNotFoundException(username);
-////
-////        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-////        for (Title title1 : title){
-////            grantedAuthorities.add(new SimpleGrantedAuthority(title1.getTitle()));
-////        }
-////
-////        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
-////    }
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//	@Autowired
+//	private IUserRepository userRepository;
+//	@Autowired
+//	private ITitleRepository titleRepository;
+//
+//	@Override
+//	@Transactional(readOnly = true)
+//	public UserDetails loadUserByUsername(String username) {
+//		User user = userRepository.findByUsername(username);
+//		List<Title> title = titleRepository.findAll();
+//
+//		if (user == null)
+//			throw new UsernameNotFoundException(username);
+//
+//		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+//		for (Title title1 : title) {
+//			grantedAuthorities.add(new SimpleGrantedAuthority(title1.getTitle()));
+//		}
+//
+//		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+//				grantedAuthorities);
+//	}
+//
+//	private List<GrantedAuthority> buildUserAuthority(Set<Title> userRoles) {
+//
+//		Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
+//
+//		// add user's authorities
+//		for (Title userRole : userRoles) {
+//			setAuths.add(new SimpleGrantedAuthority(userRole.getTitle()));
+//		}
+//
+//		List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
+//
+//		return Result;
+//	}
 //}
-=======
-package fr.formation.inti.services;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import fr.formation.inti.dao.ITitleRepository;
-import fr.formation.inti.dao.IUserRepository;
-import fr.formation.inti.entities.Title;
-import fr.formation.inti.entities.User;
-
-@Service
-public class UserDetailsServiceImpl implements UserDetailsService {
-	@Autowired
-	private IUserRepository userRepository;
-	@Autowired
-	private ITitleRepository titleRepository;
-
-	@Override
-	@Transactional(readOnly = true)
-	public UserDetails loadUserByUsername(String username) {
-		User user = userRepository.findByUsername(username);
-		List<Title> title = titleRepository.findAll();
-
-		if (user == null)
-			throw new UsernameNotFoundException(username);
-
-		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-		for (Title title1 : title) {
-			grantedAuthorities.add(new SimpleGrantedAuthority(title1.getTitle()));
-		}
-
-		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-				grantedAuthorities);
-	}
-
-	private List<GrantedAuthority> buildUserAuthority(Set<Title> userRoles) {
-
-		Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
-
-		// add user's authorities
-		for (Title userRole : userRoles) {
-			setAuths.add(new SimpleGrantedAuthority(userRole.getTitle()));
-		}
-
-		List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
-
-		return Result;
-	}
-}
->>>>>>> 917b40a0e6c3dce04843f13b87748c8c55c1eb50
+//>>>>>>> 917b40a0e6c3dce04843f13b87748c8c55c1eb50
--- client-adopt/src/app/registration/registration.component.spec.ts
+++ client-adopt/src/app/registration/registration.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignInComponent } from './sign-in.component';
+import { RegistrationComponent } from './registration.component';
 
-describe('SignInComponent', () => {
-  let component: SignInComponent;
-  let fixture: ComponentFixture<SignInComponent>;
+describe('RegistrationComponent', () => {
+  let component: RegistrationComponent;
+  let fixture: ComponentFixture<RegistrationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
+      declarations: [ RegistrationComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SignInComponent);
+    fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
