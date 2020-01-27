7e71e36b392de02383cfb61b92b384575565a103
Damien <bertinaria.damien@gmail.com>
2020-01-27 09:46:00 +0100

    2ème partie

---
+++ client-adopt/src/app/update-form/update-form.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Pet } from 'src/models/pet';
+import { PetService } from 'src/app/shared/pet/pet.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit {
+  editName: String;
+  editRace: String;
+  editAges: String;
+  idpet: number;
+  sub: Subscription;
+  pet:Pet;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private petService: PetService) {
+  }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      const id = params['idpet']; // (+) converts string 'id' to a number
+      this.petService.searchPetById(this.idpet).subscribe(pet => {
+        if (pet) {
+          this.editName = pet.name;
+          this.editRace = pet.race;
+          this.editAges = pet.age;
+          this.pet = new Pet;
+        } else {
+          this.gotoList();
+        }
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+  cancel() {
+    this.router.navigate(['/search']);
+  }
+
+  save() {
+    this.pet.name = this.editName;
+    this.pet.race = this.editRace;
+    this.pet.age = this.editAges;
+    this.petService.savePet(this.pet);
+    this.gotoList();
+  }
+
+  gotoList() {
+    if (this.person) {
+      this.router.navigate(['/search', {term: this.person.name} ]);
+    } else {
+      this.router.navigate(['/search']);
+    }
+  }
+}
