fc75758b7d9192dbf65d5a20b7ef4b00d835db94
Damien <bertinaria.damien@gmail.com>
2020-01-27 17:35:43 +0100

    10ème partie

--- Adopte_A_Pet/src/main/java/fr/formation/inti/services/JwtUserDetailsService.java
+++ Adopte_A_Pet/src/main/java/fr/formation/inti/services/JwtUserDetailsService.java
@@ -1,30 +1,41 @@
 package fr.formation.inti.services;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import fr.formation.inti.dao.ITitleRepository;
 import fr.formation.inti.dao.IUserRepository;
+import fr.formation.inti.entities.Title;
+import fr.formation.inti.entities.User;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 //c'est ici qu'on compare les users avec ceux de la BDD
 	@Autowired
 	private IUserRepository userRepository;
+	@Autowired
+	private ITitleRepository titleRepository;
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userRepository.findByUsername(username);
-		if (user != null) {
-			return new User(user.getUsername(), user.getPassword(),
-					user.getAuthorities());
-		} else {
-			throw new UsernameNotFoundException("User not found with username: " + username);
-		}
+        List<Title> title = titleRepository.findAll();
+        if (user == null) throw new UsernameNotFoundException(username);
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Title title1 : title){
+            grantedAuthorities.add(new SimpleGrantedAuthority(title1.getTitle()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
 	}
 
 }
